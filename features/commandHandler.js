b3b3354e4e35b5d633beac0c2108a20c0ae9f3af
Antonio32A <antonio.anzil@gmail.com>
2020-11-23 20:00:47 +0100

    made command loading async

--- features/commandHandler.js
+++ features/commandHandler.js
@@ -1,16 +1,19 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
 let commands = {};
-fs.readdirSync("commands").forEach(file => {
-    if (file !== "command.js") {
-        file = file.replace(".js", "");
-        let Command = require(`../commands/${file}`);
-        Command = new Command();
+fs.readdir("commands")
+    .then(files =>
+        files.forEach(file => {
+            if (file !== "command.js") {
+                file = file.replace(".js", "");
+                let Command = require(`../commands/${file}`);
+                Command = new Command();
 
-        const names = [Command.getName(), ...Command.getAliases()];
-        names.forEach(name => commands[name] = Command);
-    }
-});
+                const names = [Command.getName(), ...Command.getAliases()];
+                names.forEach(name => commands[name] = Command);
+            }
+        })
+    );
 
 const commandHandler = (author, raw, command, args) => {
     command = command.toLowerCase();
